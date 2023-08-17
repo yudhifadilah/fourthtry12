5bfec1306de2c585fb772d249f89f93c5a9c10f8
yudhifadilah <wmfkid@gmail.com>
2023-08-18 01:52:12 +0700

    hihlier

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `/${process.env.NEXT_PUBLIC_BASE_PATH}` : '',
     images: {
         loader: 'imgix',
-        path: `${process.env.NEXT_PUBLIC_BASE_PATH}/_next/static/images`
+        path: '/_next/static/images'
     }
 }
 module.exports = nextConfig
