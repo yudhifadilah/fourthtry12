15ac71330a9fe02dd7e2771148d346dfbe61c283
yudhifadilah <wmfkid@gmail.com>
2023-08-18 01:34:06 +0700

    lagidanlagi

--- src/lib/EcommerceCard.tsx
+++ src/lib/EcommerceCard.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 export function TestimonialCard() {
   return (
 <div className="flex items-center justify-center p-8 border border-black">
-  <div className="w-1/2 h-full bg-gradient-to-b from-white to-black"></div>
   <div className="w-1/2 h-full bg-white p-4">
     <h2 className="text-2xl font-semibold mb-3">Description.</h2>
     <p className="text-black-500">
