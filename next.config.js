c72bbb59019eee10c5bc11a03a1a5d2d3ef0dd0c
yudhifadilah <wmfkid@gmail.com>
2023-08-18 01:20:30 +0700

    testttttttttttt

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        loader: 'default',
+        path: `${process.env.NEXT_PUBLIC_BASE_PATH}/_next/static/images`
+    }
 }
-
-
-
-// next.config.js
 module.exports = nextConfig
-  
-  
